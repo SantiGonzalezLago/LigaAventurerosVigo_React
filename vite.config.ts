cea01cd46e05cc556c507113acec54636085ec05
Santi <santiago.gonzalez.lago@gmail.com>
2026-09-24 19:25:25 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Liga de Aventureros de Vigo',
+        short_name: 'LAVigo',
+        description: 'Plataforma y comunidad de la Liga de Aventureros de Vigo',
+        theme_color: '#121212',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '32x32',
+            type: 'image/x-icon'
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512'
+          }
+        ]
+      }
+    })
+  ],
 })
